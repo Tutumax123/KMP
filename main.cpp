69e8da450322e1ae6effb2c00b95ce902244a734
Lanyangyang127 <2774813918@qq.com>
2024-10-28 17:01:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@
 
 SqString convertToSqString(char* str) {
     SqString s;
-    s.length = strlen(str);
-    strcpy(s.data, str);
+    s.length = strlen(str); //长度
+    strcpy(s.data, str); //将字符串复制到data成员中
     return s;
 }
 //123
 int main() {
-    char setPassword[MaxSize];
-    char input[MaxSize];
-    int attempts = 3;
+    char setPassword[MaxSize]; //字符数组存储设定的密码
+    char input[MaxSize]; //存储输入的密码
+    int attempts = 3; //定义尝试次数为3次
 
     printf("请设定密码: ");
     scanf("%s", setPassword);
@@ -28,7 +28,7 @@ int main() {
         scanf("%s", input);
 
         SqString inputString = convertToSqString(input);
-
+        //使用kmp算法比较是否匹配
         if (KMP(inputString,password)) {
             printf("开锁成功！\n");
             return 0;
